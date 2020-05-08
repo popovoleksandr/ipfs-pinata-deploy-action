2c114229baf5df813a60dc7f83703a979b1f1cd4
Aleksandr Popov <anantaramdas@outlook.com>
2020-05-08 20:58:59 +0530

    Making first try to deploy to Pinata

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const pinataSDK = require('@pinata/sdk');
 
-try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+
+const sourcePath = core.getInput('path');
+const pinName = core.getInput('pin-name');
+const pinataApiKey = core.getInput('pinata-api-key');
+const pinataSecretApiKey = core.getInput('pinata-secret-api-key');
+
+const pinata = pinataSDK(pinataApiKey, pinataSecretApiKey);
+
+const options = {
+    pinataMetadata: {
+        name: pinName,
+        // keyvalues: {
+        //     customKey: 'customValue',
+        //     customKey2: 'customValue2'
+        // }
+    },
+    pinataOptions: {
+        cidVersion: 0
+    }
+};
+
+pinata.pinFromFS(sourcePath, options).then((result) => {
+    //handle results here
+    console.log(result);
+    console.log("HASH: " + result);
+    core.setOutput("hash", result.IpfsHash);
+
+}).catch((err) => {
+    //handle error here
+    console.log(err);
+});
